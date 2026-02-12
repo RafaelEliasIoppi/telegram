e187f9a64d5cdc97328d3770bc512fec54c62e7b
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2026-02-12 11:57:07 +0000

    RAFAEL

--- teste/src/test/java/telegram/teste/TesteApplicationTests.java
+++ teste/src/test/java/telegram/teste/TesteApplicationTests.java
@@ -3,7 +3,7 @@ package telegram.teste;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TesteApplication.class)
 class TesteApplicationTests {
 
 	@Test
