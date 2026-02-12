505ea79ea54535779cb610c356c11c6db2d31834
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2026-02-12 20:34:54 +0000

    rafael

--- teste/src/test/java/telegram/teste/TesteApplicationTests.java
+++ teste/src/test/java/telegram/teste/TesteApplicationTests.java
@@ -2,12 +2,15 @@ package telegram.teste;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import telegram.teste.config.TelegramServiceMockConfig;
 
 @SpringBootTest(classes = TesteApplication.class)
+@Import(TelegramServiceMockConfig.class)
 class TesteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Se o contexto subir sem erro, o teste passa
+    }
 }
