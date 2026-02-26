49dad85b2b5c5c071fd2ca944dfc35ae9e7239fa
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2026-02-26 12:40:46 +0000

    rafael

--- teste/src/main/java/telegram/teste/TesteApplication.java
+++ teste/src/main/java/telegram/teste/TesteApplication.java
@@ -2,13 +2,22 @@ package telegram.teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TesteApplication {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(TesteApplication.class, args);
+
+             // 🔹 Carrega variáveis do .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        System.setProperty("telegram.bot.token", dotenv.get("TELEGRAM_BOT_TOKEN", ""));
+        System.setProperty("telegram.chat.id", dotenv.get("TELEGRAM_CHAT_ID", ""));
+        System.setProperty("gmail.username", dotenv.get("GMAIL_USERNAME", ""));
+        System.setProperty("gmail.app.password", dotenv.get("GMAIL_APP_PASSWORD", ""));
+
+        SpringApplication.run(TesteApplication.class, args);
         System.out.println("🚀 Aplicação iniciada com sucesso!");
 
       
